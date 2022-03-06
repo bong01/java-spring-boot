7280e8a62cf9822f0590c9b0cb8f9cfddc5d87c4
lee-kyubong <46523628+bong01@users.noreply.github.com>
2022-03-06 14:10:18 +0900

    JPA-CRUD

---
+++ Chap05/jpa-crud/src/main/java/dev/bong/jpa/JpaCrudApplication.java
@@ -0,0 +1,15 @@
+package dev.bong.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class JpaCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaCrudApplication.class, args);
+    }
+
+}
---
+++ Chap05/jpa-crud/src/main/java/dev/bong/jpa/repository/PostRepository.java
@@ -0,0 +1,15 @@
+package dev.bong.jpa.repository;
+
+import dev.bong.jpa.entity.BoardEntity;
+import dev.bong.jpa.entity.PostEntity;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface PostRepository extends CrudRepository<PostEntity, Long> {
+    List<PostEntity> findAllByWriter(String writer); //where writer = ?
+
+    List<PostEntity> findAllByWriterAndBoardEntity(String writer, BoardEntity boardEntity); //where writer = ? and board_entity_id = ?
+
+    List<PostEntity> findAllByWriterContaining(String writer);
+}
