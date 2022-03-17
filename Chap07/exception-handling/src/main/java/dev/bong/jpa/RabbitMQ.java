dde40b8c26ca5a64435b503feecf951b5769b379
lee-kyubong <46523628+bong01@users.noreply.github.com>
2022-03-17 14:45:51 +0900

    Exception Handling

---
+++ Chap07/exception-handling/src/main/java/dev/bong/jpa/RabbitMQ.java
@@ -0,0 +1,22 @@
+package dev.bong.jpa;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("!prod")
+public class RabbitMQ implements MessageQueueInterface {
+    private static final Logger logger = LoggerFactory.getLogger(RabbitMQ.class);
+
+    public RabbitMQ() {
+        logger.info("rabbitmq component");
+    }
+
+    @Override
+    public String readMessage() {
+        //code for communicating with RabbitMQ
+        return "message from rabbitmq";
+    }
+}
